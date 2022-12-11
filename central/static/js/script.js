d6bffc16a706265991a5d8323823c60a23ed8576
Admilson Landim <ady.pina10@gmail.com>
2022-12-11 13:24:58 -0100

    create login and connetc to db_container
    create a login page and connect projeto to mysql deployed on docker

--- central/static/js/script.js
+++ central/static/js/script.js
@@ -1,3 +1,17 @@
 
 
-console.log("Testing =======================================================")
+const init = () => {
+
+    const inputEmail = document.querySelector('input[name="email"]');
+    const inputSenha = document.querySelector('input[name="senha"]');
+    const submitButton = document.querySelector('.login_submit');
+
+
+
+    console.log('init');
+    console.log('nome: ' + inputEmail);
+    console.log('senha: ' + inputSenha);
+}
+
+
+window.onload = init;
--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Central',
+        'USER': 'root',
+        'PASSWORD': 'central',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+        'OPTIONS': {
+            'init_command': 'Set sql_mode= "STRICT_TRANS_TABLES"'
+        }
     }
 }
 
